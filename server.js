ba29fba57f160e4aa375478ecf5c69ef980d4977
sirraddo17 <adeyemoridwansirraddo@gmail.com>
2026-09-15 21:29:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRouter = require('./routes/auth.routes');
 const walletRouter = require('./routes/wallet.routes');
 const vtpassRouter = require('./routes/vtpass.routes');
+const adminRouter = require('./routes/admin.routes');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.get('/api/health', (req, res) => res.json({ ok: true, service: 'zappi-pay-ba
 app.use('/api', authRouter);
 app.use('/api', walletRouter);
 app.use('/api', vtpassRouter);
+app.use('/api', adminRouter);
 
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
