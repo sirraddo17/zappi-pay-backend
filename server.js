c4cd8872effca0b40af933589f09441928d1f875
sirraddo17 <adeyemoridwansirraddo@gmail.com>
2026-09-15 21:16:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const authRouter = require('./routes/auth.routes');
+const walletRouter = require('./routes/wallet.routes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.get('/api/health', (req, res) => res.json({ ok: true, service: 'zappi-pay-backend' }));
 
 app.use('/api', authRouter);
+app.use('/api', walletRouter);
 
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
